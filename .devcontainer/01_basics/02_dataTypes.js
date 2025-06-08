a509fda52c3d0f568b9c893d0fa8b61865f986e8
Ari17-star <113011745+Ari17-star@users.noreply.github.com>
2025-06-08 21:16:10 +0530

    datatypes

---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict";
+
+console.log(3+3)
+
+let name = " Aritra "
+let age = 30
+let islogin = false
+
+let state;
+
+//number=> 2 to power 53
+//bigint
+//string=>""
+//boolean=>true/false
+//null =>standalone value
+//undefine=>
+//symbl=>unique
+//object=>
+console.log(typeof undefined);
+console.log(typeof null)
