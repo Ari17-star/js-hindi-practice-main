1ff870e936a46aef2312f8cbb59cd89536a33238
Ari17-star <113011745+Ari17-star@users.noreply.github.com>
2025-06-08 22:22:06 +0530

    COnversion Operation

---
+++ .devcontainer/01_basics/03_conversionOperations.js
@@ -0,0 +1,17 @@
+let score = "Aitra"
+//console.log(typeof score);
+ let valueInnumber = Number(score)
+ //console.log(typeof valueInnumber);
+//  console.log(typeof (valueInnumber));
+  //console.log(valueInnumber)
+
+  // number=> number
+  //"22abd"=> NaN
+  // true=> 1
+  //false=>0
+
+  let someNumber = 44
+
+  let stringNumber = String(someNumber);
+
+  console.log(stringNumber);
